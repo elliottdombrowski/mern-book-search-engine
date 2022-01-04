7a0a613c195807c4180c90e0d9f78ba0143777b5
elliott dombrowski <elliottroyal@me.com>
2022-01-04 13:18:54 -0600

    refactored login/signup code to use gql mutations

---
+++ client/src/utils/auth.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+
+const secret = 'secret';
+const expiration = '1h';
+
+module.exports = {
+  authMiddleware: function (req, res, next) {
+    let token = req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return res.status(400).json({ message: 'no token' });
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json({ message: 'invalid token'});
+    }
+
+    next();
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
