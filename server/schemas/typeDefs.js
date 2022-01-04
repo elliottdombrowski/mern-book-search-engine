880a35bb53c41bc29173e5c69b156b9586916e15
elliott dombrowski <elliottroyal@me.com>
2022-01-03 21:34:13 -0600

    added starter typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,21 +1,37 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    type User {
-        _id: ID!
-        name: String!
-    }
+  type User {
+    _id: ID
+    username: String!
+    email: String!
+    bookCount: TYPE?
+    savedBooks: [Book]!
+  }
 
-    type Book {
-        _id: ID!
-    }
+  type Book {
+    bookId: TYPE?
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+  }
 
-    type Query {
-        user: [User]
-        books(_id: String): [Books]
-    }
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-    type Mutation {
-        createUser(): User
-    }
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login:(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(authors: [String], description: String!, title: String!, bookId: String!, image: String, link: String): User
+    // LOOK INTO INPUT TYPE
+    removeBook(bookId: String!): User
+  }
 `;
