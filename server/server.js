db6384a7dec69694aeaa14a3d3f8398e2b3e6117
elliott dombrowski <elliottroyal@me.com>
2022-01-03 21:08:55 -0600

    edited server.js to use apollo server

--- server/server.js
+++ server/server.js
@@ -1,35 +1,37 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const { ApolloServer } = require('apollo-server-express');
- 
-// const routes = require('./routes');
+const { ApolloServer } =  require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
-app.use(express.urlencoded({ extended: true }));
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-}
+};
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-// app.use(routes);
-
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}`);
-    //PATH TO GRAPHQL PLAYGROUND
+    //CONSOLE LOG API SERVER PORT
+    console.log(`server running at port ${PORT}`);
+    //CONSOLE LOG GRAPHQL SERVER
     console.log(`GraphQL Playground running at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
